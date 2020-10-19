9dcc5922d89359d56b58c964af4d32c5e3102312
Harry Fenton <harryrfenton@gmail.com>
2020-10-19 17:21:21 +0100

    init  commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {Song} from './Song';
+function App() {
+  return (
+    <div className="App">
+     <Song />
+    </div>
+  );
+}
+
+export default App;
